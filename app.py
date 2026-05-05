d9d491ab830698d95ae823b6df5f9df0241fa654
Madeline Shi <mashi@davidson.edu>
2026-05-05 18:42:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 DB_CONFIG = {
     "host":     "localhost",
     "user":     "root",
-    "password": "gha$bbhisA23rtyuiop0999",
+    "password": "phsj7655",
     "database": "hoopbase2",
 }
 
